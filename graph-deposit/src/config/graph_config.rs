0dac736e06a3c66e268a4565a853f5ca9c1fe47c
oxide-byte <oxidebyte@gmail.com>
2025-08-31 22:01:01 +0200

    Add GraphQL PlayGround

--- graph-deposit/src/config/graph_config.rs
+++ graph-deposit/src/config/graph_config.rs
@@ -1,10 +1,10 @@
 use crate::domain::deposit::{CreateDeposit, Deposit};
 use crate::receiver::deposit_receiver::get_deposit;
-use async_graphql::http::GraphiQLSource;
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig, GraphiQLSource};
 use async_graphql::{EmptySubscription, Object, ID};
 use async_graphql_axum::{GraphQL, GraphQLRequest, GraphQLResponse};
 use axum::response::IntoResponse;
-use axum::routing::get;
+use axum::routing::{get, post_service};
 use axum::{response, Extension, Router};
 
 type Schema = async_graphql::Schema<Query, Mutation, EmptySubscription>;
@@ -45,10 +45,16 @@ async fn graphiql() -> impl IntoResponse {
     response::Html(GraphiQLSource::build().endpoint("/").finish())
 }
 
+async fn playground() -> impl IntoResponse {
+    response::Html(playground_source(GraphQLPlaygroundConfig::new("/")))
+}
+
 pub fn graph_routes() -> Router {
     let schema = Schema::build(Query, Mutation, EmptySubscription)
         .finish();
 
     Router::new()
-        .route("/", get(graphiql).post_service(GraphQL::new(schema)))
+        .route("/", post_service(GraphQL::new(schema)))
+        .route("/graphiql", get(graphiql))
+        .route("/playground", get(playground))
 }
